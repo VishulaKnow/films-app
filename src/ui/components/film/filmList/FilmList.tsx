a9fb29ff01f6fec945b4b0af73a3b58c23f820b9
Valentin Shulaev <valentin.shulaev@ics-it.ru>
2022-05-21 20:16:38 +0300

    append: no films message

--- src/ui/components/film/filmList/FilmList.tsx
+++ src/ui/components/film/filmList/FilmList.tsx
@@ -10,10 +10,20 @@ export const FilmList: React.FC<FilmListProps> = (props) => {
     return (
         <div className="film-list-wrapper">
             <div className="film-list-content">
-                {props.films.map((film) => (
-                    <FilmCard film={film} key={film.id}></FilmCard>
-                ))}
+                {props.films.length ? (
+                    props.films.map((film) => <FilmCard film={film} key={film.id}></FilmCard>)
+                ) : (
+                    <NoFilmsMessage></NoFilmsMessage>
+                )}
             </div>
+        </div>
+    );
+};
+
+export const NoFilmsMessage: React.FC = () => {
+    return (
+        <div className="film-list-empty">
+            <h3>There are not films!</h3>
         </div>
     );
 };
