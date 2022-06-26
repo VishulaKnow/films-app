61c97756bc0d3efd663c96a9137e43fb6d32f09b
Valentin Shulaev <valentin.shulaev@ics-it.ru>
2022-06-26 11:13:47 +0300

    append fetch film reducer

--- src/store/reducers/FilmsState.ts
+++ src/store/reducers/FilmsState.ts
@@ -1,26 +1,34 @@
 import { Dispatch } from "react";
 import { Action } from "redux";
-import { FilmPreview, FilmTitle } from "../../domain/film/FilmPreview";
+import { Film } from "../../domain/film/Film";
+import { FilmId, FilmPreview, FilmTitle } from "../../domain/film/FilmPreview";
 import { ImdbApiService } from "../../domain/filmDbApi/apis/imdb/ImdbApiService";
 import { ImdbUrlGenerator } from "../../domain/filmDbApi/apis/imdb/ImdbUrlGenerator";
 import { ApiService } from "../../domain/filmDbApi/ApiService";
 import { User } from "../../domain/user/User";
 
 interface FilmsSearchState {
+    currentFilm: Film | undefined | null;
     filmList: FilmPreview[] | null;
     fetchInProgress: boolean;
 }
 
 const initialState: FilmsSearchState = {
+    currentFilm: void 0,
     filmList: null,
     fetchInProgress: false
 };
 
-type FilmsActionType = "FETCH_LIST" | "START_FETCH" | "END_FETCH";
+type FilmsActionType = "FETCH_LIST" | "FETCH" | "START_FETCH" | "END_FETCH";
 
 interface FetchFilmsAction extends Action<FilmsActionType> {
     type: "FETCH_LIST";
     payload: FilmPreview[];
+}
+
+interface FetchFilmAction extends Action<FilmsActionType> {
+    type: "FETCH";
+    payload: Film | null;
 }
 
 interface StartFetchAction extends Action<FilmsActionType> {
@@ -31,7 +39,7 @@ interface EndFetchAction extends Action<FilmsActionType> {
     type: "END_FETCH";
 }
 
-type FilmsAction = FetchFilmsAction | StartFetchAction | EndFetchAction;
+type FilmsAction = FetchFilmsAction | FetchFilmAction | StartFetchAction | EndFetchAction;
 
 export function filmsSearchReducer(state = initialState, action: FilmsAction): FilmsSearchState {
     if (action.type === "FETCH_LIST") {
@@ -60,5 +68,20 @@ export function fetchFilmsListAction(title: FilmTitle, user: User | null) {
         const films = await apiService.searchFilms(title);
         dispatch({ type: "END_FETCH" });
         dispatch({ type: "FETCH_LIST", payload: films });
+    };
+}
+
+export function fetchFilmAction(id: FilmId, user: User | null) {
+    if (!user) throw new Error("User not found!");
+
+    return async (dispatch: Dispatch<FilmsAction>) => {
+        const dbService = new ImdbApiService(new ImdbUrlGenerator(user.apiKey));
+        const apiService = new ApiService(dbService);
+
+        dispatch({ type: "START_FETCH" });
+
+        const film = await apiService.fetchFilm(id);
+        dispatch({ type: "END_FETCH" });
+        dispatch({ type: "FETCH", payload: film });
     };
 }
